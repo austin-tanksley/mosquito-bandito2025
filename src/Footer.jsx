19b23e1b7d22cf2042d61818b406a4c4e8b77210
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2025-05-19 15:13:44 -0500

    footer built

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,28 @@
+import WhiteLogo from "/MosquitoBandito-Logo-White.svg"
+import { FaPhone } from "react-icons/fa6";
+import facebook from '/facebook.svg';
+import instagram from '/instagram.svg';
+import veteranOwned from '/veteran-owned.png'
+import bbb from '/bbb.png'
 
 const Footer = () => {
 
   return(
-    <div className="bg-bandito-green h-[428px]">
-      <div className="container grid">
-        <img src="" alt="Logo"></img>
-        <div className="grid grid-rows-2 grid-cols-2">
-          <p>Copyright 2025  |  Mosquito Bandito LLC</p>
+    <div className="bg-bandito-green pt-24 pb-12 flex items-center">
+      <div className="container mx-auto grid gap-2 px-4">
+        <img src={WhiteLogo} alt="Logo"></img>
+        <div className="grid grid-rows-3 grid-cols-2 sm:grid-rows-2 items-end">
+          <p className="col-start-1 col-end-3 text-tundra">© Copyright 2025  |  Mosquito Bandito LLC <br /><span className="text-bandito-green-200">Designed + Built by Rippled Studio</span></p>
+          <div className="flex gap-2 items-end col-start-1 col-end-3 sm:col-end-2">
+            <FaPhone className="text-tundra size-4"/>
+            <a href="tel:+19186880232" className="text-tundra font-body">918-688-0232</a>
+          </div>
+          <div className="flex flex-auto gap-4 items-end col-start-1 col-end-3 sm:col-start-2 sm:justify-end">
+            <a href="https://www.facebook.com/MosquitoBanditoOK/"> <img src={facebook} alt="facebook-logo" /></a>
+            <a href="https://www.instagram.com/mosquitobanditook/"><img src={instagram} alt="instagram-logo" /></a>
+            <img src={veteranOwned} alt="Veteran Owned Logo" />
+            <img src={bbb} alt="Better Business Bureau Logo" />
+          </div>
         </div>
       </div>
     </div>
--- src/Form.jsx
+++ src/Form.jsx
@@ -17,7 +17,7 @@ return (
             <input type="text" id="First-Name" className="inset-shadow-sm inset-shadow-bandito-grey bg-white p-2 rounded-lg"/>
           </div>
           <div className="form-item grid">
-            <label htmlFor="last-name" className="font-body text-bandito-grey-400 text-sm font-semibold">First Name*</label>
+            <label htmlFor="last-name" className="font-body text-bandito-grey-400 text-sm font-semibold">Last Name*</label>
             <input type="text" id="last-name" className="inset-shadow-sm inset-shadow-bandito-grey bg-white p-2 rounded-lg"/>
           </div>
           <div className="form-item grid">
@@ -25,11 +25,11 @@ return (
             <input type="email" id="email" required className="inset-shadow-sm inset-shadow-bandito-grey bg-white p-2 rounded-lg"/>
           </div>
           <div className="form-item grid">
-            <label htmlFor="Phone" className="font-body text-bandito-grey-400 text-sm font-semibold" >Phone Number*</label>
-            <input type="text" id="Phone" required className="inset-shadow-sm inset-shadow-bandito-grey bg-white p-2 rounded-lg"/>
+            <label htmlFor="phone" className="font-body text-bandito-grey-400 text-sm font-semibold" >Phone Number*</label>
+            <input type="text" id="phone" required className="inset-shadow-sm inset-shadow-bandito-grey bg-white p-2 rounded-lg"/>
           </div>
           <div className="form-item grid">
-            <label htmlFor="Street-Address" className="font-body text-bandito-grey-400 text-sm font-semibold" >Street Adress*</label>
+            <label htmlFor="street-address" className="font-body text-bandito-grey-400 text-sm font-semibold" >Street Adress*</label>
             <input type="text" id="Street-Address" required className="inset-shadow-sm inset-shadow-bandito-grey bg-white p-2 rounded-lg"/>
           </div>
           <div className="form-item grid">
@@ -42,7 +42,7 @@ return (
           </div>
           <div></div>
           <div></div>
-          <button className="justify-self-end bg-bandito-green-400 text-tundra px-6 py-2 rounded-lg border-solid border-bandito-green border-[3px]">Request</button>
+          <button className="justify-self-end bg-bandito-green-400 text-tundra px-6 py-2 rounded-lg border-solid border-bandito-green border-[3px] hover:bg-bandito-green-200 duration-500">Request</button>
         </form>
       </div>
     </div>
