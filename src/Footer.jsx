9e6fd6410d835a323d88c59439b1a38f3a8f18e2
Austin <58612782+austin-tanksley@users.noreply.github.com>
2025-05-19 08:31:15 -0500

    almost done for real this time

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,8 +3,11 @@ const Footer = () => {
 
   return(
     <div className="bg-bandito-green h-[428px]">
-      <div className="container">
-
+      <div className="container grid">
+        <img src="" alt="Logo"></img>
+        <div className="grid grid-rows-2 grid-cols-2">
+          <p>Copyright 2025  |  Mosquito Bandito LLC</p>
+        </div>
       </div>
     </div>
   )
