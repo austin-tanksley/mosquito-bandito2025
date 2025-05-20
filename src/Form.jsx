d24ffa3920a74ee0d28e13b63d02ccb83082c37f
Austin <58612782+austin-tanksley@users.noreply.github.com>
2025-05-19 22:10:47 -0500

    retrigger form detection

--- src/Form.jsx
+++ src/Form.jsx
@@ -13,7 +13,7 @@ return (
           <h2 className="  font-display text-2xl mb-4">Request a Quote</h2>
           <p className="pb-4 mr-4 border-b-4 w-fit border-bandito-red font-body ">We’ll give you a call, and schedule a good time to come out and assess your property.  </p>
         </div>
-        <form name="request quote" netlify
+        <form name="request quote" netlify method="POST"
           className="grid gap-4 md:grid-cols-2"
         >
           <div className="form-item grid">
