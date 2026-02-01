6f94c5746e8ed601bf8862ce690c7e84e074cc60
Austin Tanksley <austin.t.tanksley@gmail.com>
2026-02-01 13:05:13 -0600

    added 10 years message

--- src/About.jsx
+++ src/About.jsx
@@ -4,10 +4,15 @@ import { forwardRef } from "react"
 const About = () => {
 
     return(
-        <div
-           className=" container mx-auto p-4 py-16 grid gap-2 sm:grid-cols-2 sm:grid-rows-1 mb-8">
-            <h2 className="font-display text-3xl text-bandito-green"><span className="text-bandito-red">“</span>Hasta la vista, mosquitoes!<span className="text-bandito-red">”</span></h2>
-            <p className="type-body-semibold text-bandito-black">Mosquito Bandito has been protecting Tulsa families from pesky mosquitoes since 2016. Our fast-acting barrier treatment eliminates mosquitoes on contact and prevents new infestations for a full 3 weeks—guaranteed. Reclaim your outdoor spaces with professional mosquito protection you can trust.   </p>
+        <div>
+            <div className="container mx-auto grid py-8 font-body font-bold  text-tundra text-xl bg-bandito-green text-center sm:rounded-lg">
+                <h3>Proudly celebrating 10 years!</h3>
+            </div>
+            <div
+            className=" container mx-auto p-4 py-16 grid gap-2 sm:grid-cols-2 sm:grid-rows-1 mb-8">
+                <h2 className="font-display text-3xl text-bandito-green"><span className="text-bandito-red">“</span>Hasta la vista, mosquitoes!<span className="text-bandito-red">”</span></h2>
+                <p className="type-body-semibold text-bandito-black">Mosquito Bandito has been protecting Tulsa families from pesky mosquitoes since 2016. Our fast-acting barrier treatment eliminates mosquitoes on contact and prevents new infestations for a full 3 weeks—guaranteed. Reclaim your outdoor spaces with professional mosquito protection you can trust.   </p>
+            </div>
         </div>
     )
 };
